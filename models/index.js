7ff10eb324df6458d79c136a5478182fa25d22e1
Muhammad Nafay Khan <mnafaykhan@gmail.com>
2024-04-11 13:31:01 +0500

    resolved conflicts

--- models/index.js
+++ models/index.js
@@ -2,20 +2,11 @@ const UserModel = require("./user.model");
 const GenderModel = require("./gender.model");
 const RoleModel = require("./role.model");
 const BrandModel = require("./brand.model");
-<<<<<<< Updated upstream
-=======
-const ProductModel = require("./productModel.model");
 const CategoryModel = require("./category.model")
->>>>>>> Stashed changes
 module.exports = {
   UserModel,
   GenderModel,
   RoleModel,
-<<<<<<< Updated upstream
-  BrandModel
-=======
   BrandModel,
-  ProductModel,
   CategoryModel
->>>>>>> Stashed changes
 };
