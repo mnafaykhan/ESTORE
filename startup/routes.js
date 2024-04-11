18ddb7147c498495cb35c6cefa68e33f8c53688a
Muhammad Nafay Khan <mnafaykhan@gmail.com>
2024-04-11 12:58:22 +0500

    resolved conflicts

--- startup/routes.js
+++ startup/routes.js
@@ -1,17 +1,10 @@
-<<<<<<< Updated upstream
-const { users, brand } = require("./../routes");
-=======
 const { users, brand, model, category } = require("./../routes");
->>>>>>> Stashed changes
 
 module.exports = function (app) {
   //----------------------------------
   app.use("/api/user", users);
   app.use("/api/brand", brand);
-<<<<<<< Updated upstream
-=======
   app.use("/api/model", model);
   app.use("/api/category", category);
->>>>>>> Stashed changes
   //----------------------------------
 };
