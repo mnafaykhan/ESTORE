77162db9438f6c1f74fd2919987dd675587d2322
M-Nafay-Khan <muhammad.nafay@camp1.tkxel.com>
2024-05-06 23:38:03 +0500

    resolved conflicts with dev

--- startup/routes.js
+++ startup/routes.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-const { users, brand, model } = require("./../routes");
-=======
 const { users, brand, model, category } = require("./../routes");
->>>>>>> e1831d631d8eaffe66633caa97efc0676a5be9e6
 
 module.exports = function (app) {
   //----------------------------------
   app.use("/api/user", users);
   app.use("/api/brand", brand);
   app.use("/api/model", model);
-<<<<<<< HEAD
-=======
   app.use("/api/category", category);
->>>>>>> e1831d631d8eaffe66633caa97efc0676a5be9e6
   //----------------------------------
 };
